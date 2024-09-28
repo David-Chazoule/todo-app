8726ee6a70e89c8e5638d6a9a4e3dd3c7cfd7086
David-Chazoule <d.chazoule@gmail.com>
2024-09-28 19:20:28 +0200

    structure

---
+++ todo-app/src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+import TodoList from './TodoList'
+
+function Main() {
+  return (
+    <div>
+    < Header/>
+
+   <div>
+    <TodoList />
+   </div>
+
+    </div>
+  )
+}
+
+export default Main
